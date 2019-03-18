c7ed40c00ea07e7eb8d0e3abd19c8250ad5adca5
siddhap <siddhap@microsoft.com>
2019-03-18 22:14:00 +0530

    r

--- 1.bat
+++ 1.bat
@@ -1,5 +1,5 @@
 echo off
 for %%f in (E:\Github\acesiddhu\roslyn\artifacts\TestResults\Debug\*.xml) do (
   echo "fullname: %%f"
-C:\Users\siddhap\.nuget\packages\codecov\1.3.0\tools\codecov.exe -f %%f -t 366dc8d3-f9ca-4344-9621-25c6c1cb83ef
+C:\Users\siddhap\.nuget\packages\codecov\1.0.3\tools\codecov.exe -f %%f -t 366dc8d3-f9ca-4344-9621-25c6c1cb83ef
 )
