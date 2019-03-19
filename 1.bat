c07bee9f766a78a546cf3a9a6f655a2fe815ab2e
siddhap <siddhap@microsoft.com>
2019-03-19 15:59:03 +0530

    Used other cc file

--- 1.bat
+++
@@ -1,3 +0,0 @@
-
-
-C:\Users\siddhap\.nuget\packages\codecov\1.0.3\tools\codecov.exe -f "E:\Github\acesiddhu\roslyn\artifacts\TestResults\Debug\Microsoft.CodeAnalysis.CSharp.EditorFeatures.UnitTests.dll.8.xml" -t 366dc8d3-f9ca-4344-9621-25c6c1cb83ef
---
+++ PublishCCCodeCov.bat
@@ -0,0 +1,3 @@
+
+
+C:\Users\siddhap\.nuget\packages\codecov\1.0.3\tools\codecov.exe -f "E:\Github\acesiddhu\roslyn\artifacts\TestResults\Debug\Microsoft.CodeAnalysis.CSharp.WinRT.UnitTests.dll_net472_x86.xml" -t 366dc8d3-f9ca-4344-9621-25c6c1cb83ef
